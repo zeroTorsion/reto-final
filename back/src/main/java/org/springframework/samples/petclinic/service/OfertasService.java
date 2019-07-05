d0534fe3a4b7582379d7da77c760af2f2fa56b0f
zeroTorsion <pablonlujan@gmail.com>
2019-07-05 11:11:42 +0200

    ofertaservice

--- back/src/main/java/org/springframework/samples/petclinic/service/OfertasService.java
+++ back/src/main/java/org/springframework/samples/petclinic/service/OfertasService.java
@@ -1,6 +1,9 @@
 package org.springframework.samples.petclinic.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Ofertas;
 import org.springframework.samples.petclinic.repository.OfertasRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +13,32 @@ public class OfertasService  {
 	OfertasRepository vc_ofertasRepository;
 
 	
+	
+	public List<Ofertas> findAll(){
+		return this.vc_ofertasRepository.findAll();
+	}
+	
+	public Ofertas findById(Integer id) {
+		return this.vc_ofertasRepository.findOne(id);
+	}
+	
+	public Ofertas save(Ofertas oferta) {
+		 return this.vc_ofertasRepository.save(oferta);
+	}
 
+	public Ofertas delete(Ofertas oferta) {
+		this.vc_ofertasRepository.delete(oferta);
+		return oferta;
+	}
+
+	public void  deleteAll(){
+		this.vc_ofertasRepository.deleteAll();
+	}
+
+	public Ofertas update(Ofertas oferta) {
+		return this.vc_ofertasRepository.save(oferta);
+	}
+
+	
+	
 }
