381df77522a8871d5c3ab435ddf24bf9bb761a97
Skuldsler <RufusWein>
2019-07-05 12:09:50 +0200

    k

--- back/src/main/java/org/springframework/samples/petclinic/model/Ofertas.java
+++ back/src/main/java/org/springframework/samples/petclinic/model/Ofertas.java
@@ -4,18 +4,17 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "ofertas")
-public class Ofertas extends BaseEntity{
+public class Ofertas extends BaseEntity {
 
-    
 	@Column(name = "titulo")
 	@NotEmpty
 	private String vcTitulo;
@@ -23,84 +22,45 @@ public class Ofertas extends BaseEntity{
 	private String vcDescr;
 	@Column(name = "descuento")
 	private double vcDescu;
+	@Temporal(TemporalType.TIMESTAMP)
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
 	@Column(name = "f_expiracion")
 	private Date vcFexp;
-	
-
-
-	
 
 	public Ofertas() {
 		super();
 	}
 
-
-
-
-
 	public String getVcTitulo() {
 		return vcTitulo;
 	}
-
-
-
-
 
 	public void setVcTitulo(String vcTitulo) {
 		this.vcTitulo = vcTitulo;
 	}
 
-
-
-
-
 	public String getVcDescr() {
 		return vcDescr;
 	}
-
-
-
-
 
 	public void setVcDescr(String vcDescr) {
 		this.vcDescr = vcDescr;
 	}
 
-
-
-
-
 	public double getVcDescu() {
 		return vcDescu;
 	}
-
-
-
-
 
 	public void setVcDescu(double vcDescu) {
 		this.vcDescu = vcDescu;
 	}
 
-
-
-
-
 	public Date getVcFexp() {
 		return vcFexp;
 	}
-
-
-
-
 
 	public void setVcFexp(Date vcFexp) {
 		this.vcFexp = vcFexp;
 	}
 
-
-
-
-	
-	
 }
